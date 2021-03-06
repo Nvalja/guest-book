24301045cac04b30941f3710fcec4b636b30f868
Nazar Valchyshyn <nvalja@gmail.com>
2021-03-06 19:11:15 +0200

    styles

--- src/components/Guest/Guest.jsx
+++ src/components/Guest/Guest.jsx
@@ -11,12 +11,21 @@ export const Guest = ({ user }) => {
       >
         {user.name}
       </span>
-      <p
-        style={{color: user.textColor}}
-        className="guest__text"
-      >
-        {user.text}
-      </p>
+      <div className="guest__message">
+        <p
+          style={{color: user.textColor}}
+          className="guest__text"
+        >
+          {user.text}
+        </p>
+        <div className="guest__date">
+          <span>{user.date}</span>
+          <span>{user.time}</span>
+        </div>
+      </div>
+      
+
+
     </div>
   );
 }
--- src/components/GuestList/GuestList.jsx
+++ src/components/GuestList/GuestList.jsx
@@ -5,7 +5,6 @@ import { Guest } from '../Guest';
 export const GuestList = ({ users }) => {
   return (
     <div className="list">
-      <h2>Guest list</h2>
       {users.map(user => (
         <Guest
           key={user.id}
